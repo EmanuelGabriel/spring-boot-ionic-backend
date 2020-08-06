c6b5fc6aa9c4c6b3816db25c9caf65a008ab548c
f-mtz <mtz.dev.si@gmail.com>
2020-08-06 18:07:58 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/fmtz/cursomc/services/CategoriaService.java
+++ src/main/java/com/fmtz/cursomc/services/CategoriaService.java
@@ -21,8 +21,6 @@ public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-
-
 	
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
--- src/main/java/com/fmtz/cursomc/services/ClienteService.java
+++ src/main/java/com/fmtz/cursomc/services/ClienteService.java
@@ -1,12 +1,20 @@
 package com.fmtz.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.fmtz.cursomc.domain.Cliente;
+import com.fmtz.cursomc.domain.Cliente;
+import com.fmtz.cursomc.dto.ClienteDTO;
 import com.fmtz.cursomc.repositories.ClienteRepository;
+import com.fmtz.cursomc.services.exceptions.DataIntegrityException;
 import com.fmtz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,7 +30,49 @@ public class ClienteService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 		"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 		}
+	
+	public Cliente insert(Cliente obj) {
+		//o id nulo é pro método save entender que é uma inserção, se passassemos um id ele entenderia como uma atualização
+		obj.setId(null);
+		return repo.save(obj);
+	}
 
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+		repo.deleteById(id);
+		}
+		catch (DataIntegrityViolationException  e) {
+			throw new DataIntegrityException("Não é possível excluir pois há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);	
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+		
+	}
 	
 
 }
